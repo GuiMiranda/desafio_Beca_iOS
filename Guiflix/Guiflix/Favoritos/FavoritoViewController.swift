12c120b1490a6fb7a7bb506cb37387945351fe22
Diego Cruz <diego.123.cruz@gmail.com>
2019-10-09 17:10:30 -0300

    init project

---
+++ Guiflix/Guiflix/Favoritos/FavoritoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritoViewController.swift
+//  Guiflix
+//
+//  Created by Guilherme Miranda Lopes on 09/10/19.
+//  Copyright © 2019 Guilherme Miranda Lopes. All rights reserved.
+//
+
+import UIKit
+
+class FavoritoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Guiflix/Guiflix/Filmes/FilmesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FilmesViewController.swift
+//  Guiflix
+//
+//  Created by Guilherme Miranda Lopes on 09/10/19.
+//  Copyright © 2019 Guilherme Miranda Lopes. All rights reserved.
+//
+
+import UIKit
+
+class FilmesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
