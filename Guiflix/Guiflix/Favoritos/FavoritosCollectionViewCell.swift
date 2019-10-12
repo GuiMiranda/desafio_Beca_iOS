cb87e577c2a4160fde15d3cbd79dc36adfe05c0a
Raphael Matrone <raphaelmatrone@Raphaels-Mac-mini.local>
2019-10-12 12:57:23 -0300

    testCollections

--- Guiflix/Guiflix/Favoritos/FavoritosCollectionViewCell.swift
+++ Guiflix/Guiflix/Favoritos/FavoritosCollectionViewCell.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class FavoritosCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var favPoster: UIImageView!
+    @IBOutlet weak var favTitle: UILabel!
+    @IBOutlet weak var favYear: UILabel!
+    
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var widthConstraint: NSLayoutConstraint!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
+//        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+//        let screenWidth = UIScreen.main.bounds.size.width
+//        let screenHeight = UIScreen.main.bounds.size.height
+//        widthConstraint.constant = screenWidth - (2 * 4)
+//        heightConstraint.constant = screenHeight - (2 * 4)
+        
+        favPoster.layer.cornerRadius = 5.0
+        favPoster.layer.masksToBounds = true
 
+    }
+    
+    func setup(title: String, poster: String, year: String){
+        self.favTitle.text = title
+        self.favPoster.load(url: poster, size: .w780)
+        self.favYear.text = year
+    }
 }
+
